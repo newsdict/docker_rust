bb7b435c3a152ad8f62c343736b0dfbe571c9095
yusuke <yusuke@newsdict.jp>
2019-10-26 06:14:55 +0000

    Example for notify use slack

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,23 @@
 ARG ubuntu_version="19.10"
 FROM ubuntu:${ubuntu_version}
 
-# Dockerfile on bash
-SHELL ["/bin/sh", "-c"]
+# build name space
+ENV NAMESPACE "docker-rust"
+
+RUN apt update && apt install -y --no-install-recommends curl build-essential ca-certificates libssl-dev pkg-config
+RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+RUN /root/.cargo/bin/rustup default nightly
+
+# Prepare to build
+RUN mkdir /rust
+WORKDIR /rust
+COPY . /rust/
+RUN rm -rf target
+
+# build
+RUN /root/.cargo/bin/cargo build --release
+
+CMD target/release/$NAMESPACE
+
 
 EXPOSE 3000
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,69 @@
 
 #[macro_use] extern crate rocket;
 
+use std::collections::HashMap;
+use std::path::PathBuf;
+use std::path::Path;
+
+use rocket_contrib::json::{Json};
+use rocket_contrib::templates::Template;
+use rocket::response::NamedFile;
+
+#[macro_use]
+extern crate dotenv_codegen;
+extern crate slack_hook;
+use slack_hook::{Slack, PayloadBuilder};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct Message {
+    email: String
+}
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    let mut context = HashMap::new();
+    context.insert("name", "ok");
+    return Template::render("index", &context);
+}
+
+#[get("/js/<path..>")]
+fn js(path: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("assets/js").join(path)).ok()
+}
+
+#[get("/css/<path..>")]
+fn css(path: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("assets/css").join(path)).ok()
+}
+
+#[get("/images/<path..>")]
+fn images(path: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("assets/images").join(path)).ok()
+}
+
+#[post("/submit", data = "<message>")]
+fn submit(message: Json<Message>) -> String {
+    let slack = Slack::new(dotenv!("SLACK_WEBHOOK")).unwrap();
+    let p = PayloadBuilder::new()
+      .text(format!("email: {}", message.0.email))
+      .channel(format!("#{}", dotenv!("SLACK_CHANNEL_NAME")))
+      .username(dotenv!("SLACK_USER_NAME"))
+      .icon_emoji(":chart_with_upwards_trend:")
+      .build()
+      .unwrap();
+
+    let res = slack.send(&p);
+    if res.is_ok() {
+        return "ok".to_string();
+    } else {
+        return "ng".to_string();
+    }
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+    .mount("/", routes![index, js, css, images, submit])
+    .attach(Template::fairing())
+    .launch();
 }
